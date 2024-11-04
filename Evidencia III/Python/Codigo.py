a0240d9731040b25cbd4eb171838861bebb8f027
Mateo Bruno <mbrunobarberon@escuelasproa.edu.ar>
2024-11-04 06:12:45 -0300

    Update Codigo.py

--- Evidencia III/Python/Codigo.py
+++ Evidencia III/Python/Codigo.py
@@ -2,17 +2,24 @@ import random
 import string
 
 def generar_contrasena(longitud):
+
     caracteres = string.ascii_letters + string.digits + string.punctuation
-    contrasena = ''.join(random.choice(caracteres) for i in range(longitud))
+
+    contrasena = ''.join(random.choice(caracteres) for _ in range(longitud))
     return contrasena
 
 def main():
-    longitud = int(input("Ingrese la longitud de la contraseña: "))
-    if longitud < 8:
-        print("La longitud de la contraseña debe ser al menos 8 caracteres.")
-    else:
+
+    usuario = input("Introduce tu nombre de usuario: ")
+
+    longitud = int(input("Introduce la longitud de la contraseña: "))
+
+    if longitud >= 8:
         contrasena = generar_contrasena(longitud)
-        print("Tu contraseña segura es:", contrasena)
+        print(f"Usuario: {usuario}")
+        print(f"Tu contraseña generada es: {contrasena}")
+    else:
+        print("La longitud de la contraseña debe ser al menos 8 caracteres.")
 
 if __name__ == "__main__":
     main()
