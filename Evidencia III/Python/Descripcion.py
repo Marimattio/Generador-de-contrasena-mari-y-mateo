a81cc812a2fec8448a7c326c773f25778506cf97
Mateo Bruno <mbrunobarberon@escuelasproa.edu.ar>
2024-11-04 06:16:43 -0300

    Create Descripcion.py

---
+++ Evidencia III/Python/Descripcion.py
@@ -0,0 +1,2 @@
+Este es el codigo que se tiene que ejecutar para desarrollar la funcion de el proyecto, es un codigo que solo se puede ejecutar si se ejecuta unicamente desde el modulo principal.
+Este codigo funciona ejecutandolo desde un leector de codigo .Py
